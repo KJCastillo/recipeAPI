2bc758b55c0113867b6a49c1bdcea2db7335be30
Kevin Castillo <kevincastillo318@gmail.com>
2021-11-02 16:42:03 -0700

    api key and app id made

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import Body from "./components/Body";
 import Footer from "./components/Footer";
 
 export const App = () => {
+
+  const appID = "b987e4bd"
+  const apiKey = "cdf7a13223282b8719e9fe4b8216e19e"
+  const apiRequest = 
+  `https://api.edamam.com/search?q=chicken&app_id=${appID}&app_key=${apiKey}`
+  //used backticks to insert appID and apiKey
+  
   return (
     <div className="App ">
       <Navbar />
